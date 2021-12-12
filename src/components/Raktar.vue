45edb1d700df5a1f1132198b7dc97eac34958252
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-12-12 15:27:28 +0100

    táblázat kész

---
+++ src/components/Raktar.vue
@@ -0,0 +1,33 @@
+<template>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Quantity</th>
+        </tr>
+        <tr v-for="adat in adatok" :key="adat.title">
+            <td> {{adat.title}} </td>
+            <td> {{adat.price}} </td>
+            <td> {{adat.quantity}} </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+    props: ['adatok']
+}
+</script>
+
+<style scoped>
+    table {
+        width: 50%;
+        margin: auto;
+        border: 2px solid;
+        text-align: center;
+    }
+
+    th {
+        border-bottom: 2px solid black;
+    }
+</style>
